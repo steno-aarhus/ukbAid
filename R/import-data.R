f64e09a3531f04cca1803e2235d5283a8ab8f020
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-02 20:53:41 +0200

    small fixes and have data be saved as data-user-project

--- R/import-data.R
+++ R/import-data.R
@@ -12,12 +12,14 @@ dare_project_record_id <- "record-G9zGPB0JbxZz4xPy60PBPk1Z"
 #' @param variables_to_extract A character vector of variables you want to
 #'   extract from the UKB database (from the `.dataset` file). Use
 #'   `save_database_variables_to_project()` to create the
-#'   `data-raw/variables.csv` file, open that file, and delete all variables you
+#'   `data-raw/rap-variables.csv` file, open that file, and delete all variables you
 #'   don't want to keep. This is the file that contains the `variable_name` column
 #'   you would use for this argument.
 #' @param dataset_record_id The "record ID" of the database, found when clicking
 #'   the `.dataset` file in the RAP project folder. Defaults to the ID for the
 #'   Steno project `dare_project_record_id`.
+#' @param project_id The project's abbreviation. Defaults to using
+#'   `get_project_id()`, which is the name of the project folder.
 #'
 #' @return Outputs whether the extraction and creation of the data was
 #'   successful or not. Used for the side effect of creating the CSV on the RAP
@@ -28,11 +30,11 @@ dare_project_record_id <- "record-G9zGPB0JbxZz4xPy60PBPk1Z"
 #'
 #' \dontrun{
 #' library(tidyverse)
-#' read_csv("data-raw/variables.csv") %>%
+#' read_csv("data-raw/rap-variables.csv") %>%
 #'   pull(variable_name) %>%
 #'   create_csv_from_database()
 #' }
-create_csv_from_database <- function(variables_to_extract, dataset_record_id = dare_project_record_id) {
+create_csv_from_database <- function(variables_to_extract, project_id = get_project_id(), dataset_record_id = dare_project_record_id) {
     stopifnot(is.character(dataset_record_id), is.character(variables_to_extract))
 
     table_exporter_options <- list(
@@ -41,16 +43,27 @@ create_csv_from_database <- function(variables_to_extract, dataset_record_id = d
         header_style = "FIELD-TITLE"
     )
 
-    user_name <- system("dx whoami", intern = TRUE)
+    user_name <- get_username()
     table_exporter_options <- rjson::toJSON(table_exporter_options)
     table_exporter_command <- glue::glue(
-        "dx run app-table-exporter -ientity=participant --brief --wait -y -j '{table_exporter_options}' -ioutput=data-{user_name}"
+        "dx run app-table-exporter -ientity=participant --brief --wait -y -j '{table_exporter_options}' -ioutput=data-{user_name}-{project_id}"
     )
     cli::cli_alert_info("Started extracting the variables and converting to CSV.")
+    cli::cli_alert_warning("This function runs for quite a while, at least 5 minutes or more. Please be patient to let it finish.")
     table_exporter_results <- system(table_exporter_command, intern = TRUE)
     cli::cli_alert_success("Finished saving to CSV. Check {.val /mnt/project/} or the project folder on the RAP to see that it was created.")
     relevant_results <- tail(table_exporter_results, 3)[1:2]
     return(relevant_results)
+}
+
+#' Title
+#'
+#' @return The project abbreviation/id, which is the name of the project folder
+#'   (and the same as the name of the `.Rproj` folder).
+#' @export
+#'
+get_project_id <- function() {
+    fs::path_file(usethis::proj_path())
 }
 
 #' Get the username of the user's who started the RStudio session.
@@ -69,12 +82,13 @@ get_username <- function() {
 #' you'd need to download it every time you go back to analyzing the data.
 #'
 #' @param username A RAP username. Defaults to using `get_username()`.
+#' @inheritParams create_csv_from_database
 #'
 #' @return Downloads CSV to `data/data.csv`.
 #' @export
 #'
-download_project_data <- function(username = get_username()) {
-    download_command <- glue::glue("dx download data-{username}.csv --output data/data.csv")
+download_project_data <- function(project_id = get_project_id(), username = get_username()) {
+    download_command <- glue::glue("dx download data-{username}-{project_id}.csv --output data/data.csv")
     cli::cli_alert_info("Downloading data to {.val data/data.csv}.")
     system(download_command)
     cli::cli_alert_success("Downloaded CSV!")
