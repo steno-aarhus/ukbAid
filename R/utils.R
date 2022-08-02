70e7aed139a21ed247679a1d4f47d5c99969dd2b
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-02 21:49:40 +0200

    don't use here::here within commit function

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 #' Add and commit a file to the Git history.
 #'
-#' @param file File to commit. Uses [here::here()] to reference file.
+#' @param file File to commit.
 #' @param message Commit message.
 #'
 #' @return Nothing. Adds and commits a file.
@@ -8,7 +8,7 @@
 #'
 git_commit_file <- function(file, message) {
     stopifnot(is.character(file), is.character(message))
-    gert::git_add(here::here(file))
+    gert::git_add(file)
     gert::git_commit(message)
     return(invisible())
 }
