eddb1ecea6715586d50edcf3ee30747528704cbc
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-02 20:54:36 +0200

    documentation for variables dataframe

---
+++ R/ukbAid-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+#' All variables found within the UK Biobank
+#'
+#' A dataframe that contains the field IDs and variable names for the over 8000
+#' variables within the UK Biobank.
+#'
+#' @format A data frame with 8272 rows and 3 variables:
+#' \describe{
+#'   \item{field_id}{The ID for the "field" (or variable).}
+#'   \item{variable_name}{Full name of the variable.}
+#'   \item{link}{URL to the description of the variable.}
+#' }
+#' @source \url{https://biobank.ndph.ox.ac.uk/~bbdatan/Data_Dictionary_Showcase.tsv}
+"variables"
