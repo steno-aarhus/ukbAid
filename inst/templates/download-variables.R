4fa34f9e31ea3c6a755d9dcf657dd9b3f464312c
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-11 16:07:28 +0200

    don't need anymore

--- inst/templates/download-variables.R
+++
@@ -1,9 +0,0 @@
-
-# Script to download and save the RAP variables to data-raw/ --------------
-
-# Really only need to run this script once.
-
-# This function will save the list of variables in the database into this `data-raw/`
-# folder as `rap-variables.csv`.
-ukbAid::download_database_variables()
-ukbAid::git_commit_database_variables()
