17ae77640449f9b0879883ec05a67fc79f9b52f0
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-12 15:49:01 +0200

    added instance filtering to RAP variables csv

--- R/import-variables.R
+++ R/import-variables.R
@@ -63,22 +63,30 @@ import_clean_and_upload_database_variables <- function() {
 #'
 #' @param project_variables_file The file location for the project variable CSV file. Defaults to `data-raw/project-variables.csv`.
 #' @param rap_variables_file The file location for the RAP specific variable CSV file. Defaults to `data-raw/rap-variables.csv`.
+#' @param instances An "instance" in UK Biobank is a time point of collection
+#'   for the variable. So a variable with Instance 0 and Instance 1 means that
+#'   the variable was collected twice at two different times. The default is to
+#'   select all the time points. Max is 9 instances, and while many variables
+#'   have up to 4 instances, the majority only have one (e.g. 0).
 #' @param save Whether to save (and overwrite) the newly subsetted RAP variables.
 #'
 #' @return A tibble of the newly subsetted RAP variable list. Save (overwrite) original RAP csv file.
 #' @export
 #'
 subset_rap_variables <- function(project_variables_file = "data-raw/project-variables.csv",
-                                 rap_variables_file = "data-raw/rap-variables.csv", save = TRUE) {
+                                 rap_variables_file = "data-raw/rap-variables.csv", instances = 0:9, save = TRUE) {
+    instance_pattern <- glue::glue("Instance [{min(instances)}-{max(instances)}]")
     proj_vars <- readr::read_csv(here::here(project_variables_file), show_col_types = FALSE)
-    rap_vars <- readr::read_csv(here::here(rap_variables_file), show_col_types = FALSE)
+    rap_vars <- readr::read_csv(here::here(rap_variables_file), show_col_types = FALSE) %>%
+        dplyr::filter(stringr::str_detect(rap_variable_name, instance_pattern))
 
     new_rap_vars <- rap_vars %>%
         dplyr::mutate(id = stringr::str_extract(field_id, "^p[:digit:]+")) %>%
         dplyr::right_join(proj_vars, by = "id") %>%
-        dplyr::select(-id)
+        dplyr::select(-id, -link, -ukb_variable_description)
 
     if (save) {
+        cli::cli_alert_info("Updated the {.val {rap_variables_file}} based on the selected project variables.")
         readr::write_csv(new_rap_vars, here::here(rap_variables_file))
     }
 
