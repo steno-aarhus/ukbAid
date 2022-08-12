bfaa8be68946f4fc69c96de662f148dce52c9aae
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-12 15:50:21 +0200

    these are needed to run when uncommented

--- inst/templates/download-data.R
+++ inst/templates/download-data.R
@@ -1,6 +1,8 @@
 
 # Keep only the necessary variables for RAP -------------------------------
 
+library(magrittr)
+
 # After the variables have been properly selected in the `data-raw/project-variables.csv`
 # file, run this function so that only the selected variables are kept in the
 # `data-raw/rap-variables.csv` file. This file has the exact variable names used
@@ -14,8 +16,8 @@ ukbAid::subset_rap_variables()
 # Uncomment and run *ONLY AFTER* running the above function. After running this
 # code and creating the csv file in the main RAP project folder, comment it
 # out again so you don't run it anymore.
-# read_csv(here::here("data-raw/rap-variables.csv")) %>%
-#     pull(variable_name) %>%
+# readr::read_csv(here::here("data-raw/rap-variables.csv")) %>%
+#     dplyr::pull(rap_variable_name) %>%
 #     ukbAid::create_csv_from_database()
 
 # Download the project data from RAP into the session ---------------------
