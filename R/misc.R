82e5dc805184b6db5eceb2d40c23e573c724d51b
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-12 15:49:17 +0200

    add ukb statements. Closes #1

---
+++ R/misc.R
@@ -0,0 +1,2 @@
+ukb_protocol_statement <- "This research will use the UK Biobank Resource under Application Number 81520."
+ukb_manuscript_statement <- "This research has been conducted using the UK Biobank Resource under Application Number 81520."
