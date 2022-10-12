ead21421de8fbfbe8aaf3d2c1ae2881c8cc292b4
Luke W. Johnston <lwjohnst@gmail.com>
2022-10-12 13:57:24 -0400

    switch to extract schema directly (it is more often updated)

--- data-raw/variables.R
+++ data-raw/variables.R
@@ -1,41 +1,35 @@
 library(tidyverse)
 
 # Download data dictionary to get variables, links, and detailed variable description
+# List of schemas are here: https://biobank.ndph.ox.ac.uk/ukb/download.cgi
+# This schema is from here: https://biobank.ndph.ox.ac.uk/ukb/schema.cgi?id=1
 ukb_variables <-
     read_tsv(
-        "https://biobank.ndph.ox.ac.uk/~bbdatan/Data_Dictionary_Showcase.tsv",
+        "https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=1",
         col_types = cols_only(
-            FieldID = col_double(),
-            Field = col_character(),
-            Participants = col_double(),
-            Instances = col_double(),
-            Array = col_double(),
-            Link = col_character()
+            field_id = col_double(),
+            title = col_character(),
+            num_participants = col_double(),
+            instanced = col_double(),
+            arrayed = col_double(),
+            instance_min = col_double(),
+            instance_max = col_double(),
+            array_min = col_double(),
+            array_max = col_double()
         )
-    ) %>%
-    rename_with(snakecase::to_snake_case)
+    )
 
 # Convert into a format that RAP can understand. First chunking to use furrr
 # for parallel processing since it can sometimes take a while.
 rap_variables_chunked <- ukb_variables %>%
-    # To have first instance/array be 0 (zero), not one (because of RAP setup).
-    mutate(across(c(instances, array), ~ . - 1)) %>%
-    mutate(
-        instanced = instances >= 1,
-        arrayed = array >= 1,
-        instance_max = instances,
-        instance_min = 0,
-        array_max = array,
-        array_min = 0
-    ) %>%
-    select(-participants, -link, -array, -instances) %>%
+    select(-num_participants) %>%
     pivot_longer(
-        cols = -c(field_id, field, instanced, arrayed),
+        cols = -c(field_id, title, instanced, arrayed),
         names_to = c(".value", "range"),
         names_pattern = "^(.*)_(.*)$"
     ) %>%
     select(-range) %>%
-    group_split(field_id, field, instanced, arrayed)
+    group_split(field_id, title, instanced, arrayed)
 
 create_rap_specific_variables_and_ids <- function(data) {
     data %>%
@@ -43,7 +37,7 @@ create_rap_specific_variables_and_ids <- function(data) {
             instance = tidyr::full_seq(instance, 1),
             array = tidyr::full_seq(array, 1)
         ) %>%
-        tidyr::fill(field_id, field, instanced, arrayed) %>%
+        tidyr::fill(field_id, title, instanced, arrayed) %>%
         dplyr::mutate(
             field_id = dplyr::case_when(
                 instanced &
@@ -57,33 +51,33 @@ create_rap_specific_variables_and_ids <- function(data) {
             ),
             field = dplyr::case_when(
                 instanced &
-                    arrayed ~ glue::glue("{field} | Instance {instance} | Array {array}"),
+                    arrayed ~ glue::glue("{title} | Instance {instance} | Array {array}"),
                 instanced &
-                    !arrayed ~ glue::glue("{field} | Instance {instance}"),
+                    !arrayed ~ glue::glue("{title} | Instance {instance}"),
                 !instanced &
-                    arrayed ~ glue::glue("{field} | Array {array}"),
+                    arrayed ~ glue::glue("{title} | Array {array}"),
                 !instanced &
-                    !arrayed ~ glue::glue("{field}")
+                    !arrayed ~ glue::glue("{title}")
             )
         ) %>%
         dplyr::ungroup() %>%
-        dplyr::distinct(field_id, field,)
+        dplyr::distinct(field_id, title)
 }
 
 library(furrr)
 plan(multisession)
 rap_variables <- future_map_dfr(rap_variables_chunked,
                                 create_rap_specific_variables_and_ids) %>%
-    rename(rap_variable_name = field)
+    rename(rap_variable_name = title)
 plan(sequential)
 
 # Include a dataframe of variables that are NOT in the very long format.
 project_variables <- ukb_variables %>%
     select(id = field_id,
-           ukb_variable_description = field,
-           sample_size = participants,
-           link) %>%
-    mutate(id = glue::glue("p{id}")) %>%
+           ukb_variable_description = title,
+           number_participants = num_participants) %>%
+    mutate(id = glue::glue("p{id}"),
+           link = glue::glue("https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id={id}")) %>%
     distinct()
 
 usethis::use_data(rap_variables, project_variables, overwrite = TRUE, version = 3, internal = TRUE)
