c552c8279e2d0db95ea465a1da5b0d620d776abb
Luke W. Johnston <lwjohnst@gmail.com>
2023-09-06 18:04:26 +0200

    functions to upload data to RAP and to read parquet. Closes #17 and closes #16

--- R/import-data.R
+++ R/import-data.R
@@ -5,29 +5,52 @@
 #' use the UKB RAP. We don't want to save the data within the Git repository, so
 #' you'd need to download it every time you go back to analyzing the data.
 #'
-#' @param username A RAP username. Defaults to using `get_username()`.
+#' @param path The file path to the dataset you want to upload.
 #' @inheritParams create_csv_from_database
 #'
 #' @return Downloads CSV to `data/data.csv`.
 #' @export
 #'
-download_project_data <- function(project_id = get_rap_project_id(), username = get_username()) {
-  download_command <- glue::glue("dx download /users/{username}/data-{project_id}.csv --output data/data.csv")
+download_project_data <- function(path,
+                                  project_id = get_rap_project_id(),
+                                  file_prefix = "data",
+                                  username = get_username()) {
+  download_command <- glue::glue("dx download /users/{username}/{file_prefix}-{project_id}.csv --output data/data.csv")
   cli::cli_alert_info("Downloading data to {.val data/data.csv}.")
   system(download_command)
   cli::cli_alert_success("Downloaded CSV!")
   return(here::here("data/data.csv"))
 }
 
-upload_csv_as_parquet <- function(csv_path, project_id = get_rap_project_id(), username = get_username()) {
-  parquet_path <- parquetize::csv_to_parquet(
-    path_to_file = csv_path,
-    path_to_parquet = fs::path_ext_set(csv_path, ".parquet")
-  )
-
-  upload_command <- glue::glue("dx upload {parquet_path} --destination /users/{username}/data-{project_id}.parquet")
-  cli::cli_alert_info("Uploaded data to {.val }.")
+#' Upload a data file to the RAP.
+#'
+#' @param path Path to the dataset you want to upload.
+#' @inheritParams create_csv_from_database
+#'
+#' @return A path to the uploaded data file.
+#' @export
+#'
+upload_data <- function(path,
+                        project_id = get_rap_project_id(),
+                        file_prefix = "data",
+                        username = get_username()) {
+  rap_path <- glue::glue("/users/{username}/{file_prefix}-{project_id}.{fs::path_ext(path)}")
+  upload_command <- glue::glue("dx upload {path} --destination {rap_path}")
+  cli::cli_alert_info("Uploading data to {.val {rap_path}}.")
   system(download_command)
-  cli::cli_alert_success("Downloaded CSV!")
-  return(here::here("data/data.csv"))
+  cli::cli_alert_success("Uploaded the file to the RAP!")
+  return(rap_path)
+}
+
+#' Read a Parquet file and convert to DuckDB format.
+#'
+#' @param path Path to the Parquet file.
+#'
+#' @return A SQL database object.
+#' @export
+#'
+read_parquet <- function(path) {
+  path %>%
+    arrow::open_dataset() %>%
+    arrow::to_duckdb()
 }
