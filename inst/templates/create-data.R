545bd3a2038072d206dc8f4248a2d8e2726bbc9e
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-30 23:21:01 +0200

    chore: update the template create-data file

--- inst/templates/create-data.R
+++ inst/templates/create-data.R
@@ -3,30 +3,21 @@
 
 library(magrittr)
 
-# After the variables have been properly selected in the `data-raw/project-variables.csv`
-# file, run this function so that only the selected variables are kept in the
-# `data-raw/rap-variables.csv` file. This file has the exact variable names used
-# by RAP that we need in order to create the project-specific dataset. After
-# running this function, review the changes in Git and add and commit the changed
-# files into the history.
+# Select the RAP variables you want by deleting rows in the `data-raw/rap-variables.csv`
+# file. Run the commented function below if you want to start over (I suggest
+# you keep a copy or make sure to track edits to the file in Git).
 
 # Uncomment if you messed up and need to start over.
-# ukbAid::project_variables %>%
-#     readr::write_csv(here::here("data-raw/project-variables.csv"))
-
-# Update if necessary.
 # ukbAid::rap_variables %>%
 #     readr::write_csv(here::here("data-raw/rap-variables.csv"))
 
-ukbAid::subset_rap_variables(instances = 0:9)
-
 # Create the project dataset and save inside RAP --------------------------
 
-# Uncomment and run the below lines **ONLY AFTER** running the above function.
+# Uncomment and run the below lines **ONLY AFTER** selecting the variables from above.
 # After running this code and creating the csv file in the main RAP project
 # folder, comment it out again so you don't accidentally run it anymore (unless
 # you need to re-create the dataset).
 
 # readr::read_csv(here::here("data-raw/rap-variables.csv")) %>%
-#     dplyr::pull(rap_variable_name) %>%
+#     dplyr::pull(id) %>%
 #     ukbAid::create_csv_from_database()
