7e5d218aeabe7e1d9ff983a257259ed81f206488
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-30 23:21:33 +0200

    refactor: use the base pipe

--- data-raw/variables.R
+++ data-raw/variables.R
@@ -4,7 +4,7 @@ rap_get_path_schema() |>
   rap_copy_from("data-raw/schema.json.gz") |>
   R.utils::gunzip(overwrite = TRUE)
 
-fields <- jsonlite::read_json("data-raw/schema.json")$model$entities$participant$fields %>%
+fields <- jsonlite::read_json("data-raw/schema.json")$model$entities$participant$fields |>
   unname()
 
 rap_variables <- tibble::tibble(
