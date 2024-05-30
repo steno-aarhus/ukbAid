45cf2a3b427e5f19ebf5451aa3cec1582f8f1a2e
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-30 23:20:26 +0200

    docs: add roxygen docs to rap functions

--- R/rap.R
+++ R/rap.R
@@ -20,25 +20,61 @@ get_username <- function() {
   rap_get_user()
 }
 
+#' Get the paths of all the users within the RAP project server.
+#'
+#' @return Character vector of folder paths.
+#' @export
+#'
 rap_get_path_users <- function() {
   rap_get_path_files("users/")
 }
 
+#' Get the path of a single user within the RAP project server.
+#'
+#' @param user A user's username.
+#'
+#' @return A character scalar of one users path.
+#' @export
+#'
 rap_get_path_user <- function(user) {
-  rap_get_path_users() |>
+  users <- rap_get_path_users()
+  user <- rlang::arg_match(
+    user,
+    stringr::str_remove(users, "/$")
+  )
+
+  users |>
     stringr::str_subset(user)
 }
 
+#' Title
+#'
+#' @param path
+#'
+#' @return
+#' @export
+#'
+#' @examples
 rap_get_path_files <- function(path) {
   run_dx(c("ls", "--all", path)) |>
-    stringr::str_remove("^\\.\\.?/$")
+    stringr::str_subset("^\\.\\.?/$", negate = TRUE)
 }
 
+#' Get the path to the RAP UK Biobank dataset.
+#'
+#' @return A path as a scalar character.
+#' @export
+#'
 rap_get_path_database <- function() {
   rap_get_path_files(".") |>
     stringr::str_subset("\\.dataset$")
 }
 
+#' Get the path to the RAP UK Biobank schema.
+#'
+#' @return A path as a scalar character.
+#' @export
+#'
 rap_get_path_schema <- function() {
   rap_get_path_files(".") |>
     stringr::str_subset("\\.json\\.gz")
@@ -46,11 +82,27 @@ rap_get_path_schema <- function() {
 
 # Copying -----------------------------------------------------------------
 
+#' Copying files between the RAP server and the RStudio RAP environment.
+#'
+#' @name rap_copy
+#' @param local_path The path within the RStudio environment in the RAP.
+#' @param rap_path The path within the RAP project server.
+#'
+#' @return The path of the copied file.
+#'
+NULL
+
+#' @describeIn rap_copy Copying from the "local" RStudio environment on the RAP
+#'   and into the RAP project server.
+#' @export
 rap_copy_to <- function(local_path, rap_path) {
   run_dx(c("upload", local_path, "--destination", rap_path))
   rap_path
 }
 
+#' @describeIn rap_copy Copying from the RAP project server and into the "local"
+#'   RStudio environment on the RAP.
+#' @export
 rap_copy_from <- function(rap_path, local_path) {
   run_dx(c("download", rap_path, "--output", local_path,
            "--overwrite", "--no-progress", "--lightweight"))
