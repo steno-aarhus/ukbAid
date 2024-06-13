d99c9005bd59c17eb966b412b39942cfc892bb0a
Luke W. Johnston <lwjohnst@gmail.com>
2024-06-13 20:08:02 +0200

    feat: rebuild the csv creator function from scratch

--- R/proj.R
+++ R/proj.R
@@ -4,10 +4,21 @@
 #'   (and the same as the name of the `.Rproj` folder).
 #' @export
 #'
-get_rap_project_id <- function() {
+proj_get_name <- function() {
   options(usethis.quiet = TRUE)
   on.exit(options(usethis.quiet = NULL))
   fs::path_file(usethis::proj_path())
+}
+
+#' @describeIn proj_get_name Deprecated function. Use `proj_get_name()` instead.
+#' @export
+get_rap_project_id <- function() {
+  lifecycle::deprecate_soft(
+    when = "0.1.0",
+    what = "get_rap_project_id()",
+    with = "proj_get_name()"
+  )
+  proj_get_name()
 }
 
 #' Get package dependencies from the DESCRIPTION file.
@@ -15,7 +26,85 @@ get_rap_project_id <- function() {
 #' @return A character vector of package names.
 #' @export
 #'
-get_proj_dependencies <- function() {
+proj_get_dependencies <- function() {
   desc::desc_get_deps()$package |>
     stringr::str_subset("^R$", negate = TRUE)
+}
+
+#' @describeIn proj_get_dependencies Deprecated function. Use `proj_get_dependencies()` instead.
+#' @export
+get_proj_dependencies <- function() {
+  lifecycle::deprecate_soft(
+    when = "0.1.0",
+    what = "get_proj_dependencies()",
+    with = "proj_get_dependencies()"
+  )
+  proj_get_dependencies()
+}
+
+# proj_get_git_config()
+# proj_get_rstudio_config()
+# proj_setup_git_config()
+# proj_setup_rstudio_config()
+
+proj_create_path_database <- function(user) {
+  user <- rlang::arg_match(
+    user,
+    rap_get_path_users() |>
+      stringr::str_remove("/$") |>
+      stringr::str_remove("^/users/")
+  )
+  project_id <- proj_get_name()
+  timestamp_now <- lubridate::now() |>
+    lubridate::format_ISO8601() |>
+    stringr::str_replace_all("[:]", "-")
+  glue::glue("{user}-{project_id}-{timestamp_now()}.csv")
+}
+
+
+#'
+#' This function tells RAP to extract the variables you want from the `.dataset`
+#' database file and to create a CSV file within the main RAP project folder.
+#' When you want to use the CSV file in your own data analysis project, use
+#' `download_project_data()`. You probably don't need to run this function often,
+#' probably only once at the start of your project. NOTE: This function takes
+#' 5-60 minutes to run on the UKB RAP, so be careful with just randomly running it.
+#'
+#' @param fields A character vector of field IDs you want to extract from the
+#'   UKB database. Select the variable ids from the [rap_variables] dataset.
+#' @param output_path The name of the output CSV file.
+#' @param database_id The ID of the dataset, defaults to the one we use in the
+#'   Steno UK Biobank DARE project.
+#'
+#' @return Outputs character string of the results from the command sent to RAP.
+#'
+#' @examples
+#' \dontrun{
+#' proj_create_database(c("eid", "p31", "p34"), "test-data.csv")
+#' }
+proj_create_database <- function(fields,
+                                 output_path,
+                                 database_id = dare_project_record_id) {
+  checkmate::assert_character(fields)
+  checkmate::assert_character(database_id)
+  checkmate::assert_scalar(database_id)
+  checkmate::assert_character(output_path)
+  checkmate::assert_scalar(output_path)
+
+  fields <- paste0(glue::glue("-ifield_names={fields}"), collapse = " ")
+
+  cli::cli_alert_info("Sent request to RAP to create a subset of the database into the {.val {output_path}} file.")
+  cli::cli_alert_warning("This function can take a while. You can watch it in RAP Monitor.")
+  run_dx(c(
+    "run",
+    "app-table-exporter",
+    "--brief",
+    "--priority", "high",
+    "-y",
+    # raw to reduce runtime
+    "-icoding_option=raw",
+    fields,
+    glue::glue("-idataset_or_cohort_or_dashboard={database_id}"),
+    glue::glue("-ioutput={fs::path_ext_remove(output_path)}")
+  ))
 }
