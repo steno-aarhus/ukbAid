2188c9047381fbdebdbe888a41f94f9a2bdd33f5
Luke W. Johnston <lwjohnst@gmail.com>
2024-06-13 20:34:11 +0200

    fix: this shouldn't error right away, want to use the custom message instead

--- R/utils.R
+++ R/utils.R
@@ -17,7 +17,8 @@ git_commit_file <- function(file, message) {
 verify_cli <- function(program, error, call = rlang::caller_env()) {
   output <- processx::run(
     "which",
-    program
+    program,
+    error_on_status = FALSE
   )
   if (output$status) {
     cli::cli_abort(error, call = call)
