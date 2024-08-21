982977afbbbbd75deb2aea4eb2786fcad201ccfd
Luke W. Johnston <lwjohnst@gmail.com>
2024-08-21 17:59:12 +0200

    fix: finally got this function to generate data faster and without issues (so far)

--- R/proj.R
+++ R/proj.R
@@ -1,6 +1,5 @@
 # Get ---------------------------------------------------------------------
 
-
 #' Get the RAP project ID, will be the project main folder.
 #'
 #' @return The project abbreviation/id, which is the name of the project folder
@@ -114,32 +113,24 @@ proj_setup_rap <- function() {
 
 # Create ------------------------------------------------------------------
 
-#' Create a file name unique for a project and user, along with timestamp.
+#' Create a file name unique for a project, along with timestamp.
 #'
 #' You use this when you want to create a dataset when using [proj_create_database()].
-#'
-#' @param user The name of the user you are creating this dataset for.
 #'
 #' @return A character scalar.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' proj_create_path_dataset("lwjohnst")
+#' proj_create_path_dataset()
 #' }
-proj_create_path_dataset <- function(user) {
+proj_create_path_dataset <- function() {
   verify_dx()
-  user <- rlang::arg_match(
-    user,
-    rap_get_path_users() |>
-      stringr::str_subset("/\\.?\\.?$", negate = TRUE) |>
-      stringr::str_remove("^/users/")
-  )
   project_id <- proj_get_name()
   timestamp_now <- lubridate::now() |>
     lubridate::format_ISO8601() |>
     stringr::str_replace_all("[:]", "-")
-  glue::glue("{user}-{project_id}-{timestamp_now()}.csv")
+  glue::glue("{project_id}-{timestamp_now()}.csv")
 }
 
 #' Extract the variables you want from the UKB database and create a CSV dataset.
@@ -147,15 +138,14 @@ proj_create_path_dataset <- function(user) {
 #' This function tells RAP to extract the variables you want from the `.dataset`
 #' database file and to create a CSV file within the main RAP project folder.
 #' When you want to use the CSV file in your own data analysis project, use
-#' `download_project_data()`. You probably don't need to run this function often,
+#' `rap_copy_from()`. You probably don't need to run this function often,
 #' probably only once at the start of your project. NOTE: This function takes
 #' 5-60 minutes to run on the UKB RAP, so be careful with just randomly running it.
 #'
 #' @param fields A character vector of field IDs you want to extract from the
 #'   UKB database. Select the variable ids from the [rap_variables] dataset.
 #' @param output_path The name of the output CSV file.
-#' @param database_id The ID of the dataset, defaults to the one we use in the
-#'   Steno UK Biobank DARE project.
+#' @param database_id The ID of the dataset, found using [rap_get_database_id()].
 #'
 #' @return Outputs character string of the results from the command sent to RAP.
 #' @export
@@ -164,12 +154,12 @@ proj_create_path_dataset <- function(user) {
 #' \dontrun{
 #' proj_create_dataset(
 #'   c("eid", "p31", "p34"),
-#'   proj_create_path_dataset(rap_get_user())
+#'   proj_create_path_dataset()
 #' )
 #' }
 proj_create_dataset <- function(fields,
                                 output_path,
-                                database_id = dare_project_record_id) {
+                                database_id = rap_get_database_id()) {
   checkmate::assert_character(fields)
   checkmate::assert_character(database_id)
   checkmate::assert_scalar(database_id)
@@ -187,7 +177,9 @@ proj_create_dataset <- function(fields,
     "--instance-type", "mem2_ssd2_v2_x8",
     "--priority", "high",
     "-y",
-    "-ientity=eid",
+    # To reduce compute time
+    "-icoding_options=RAW",
+    "-ientity=participant",
     fields,
     glue::glue("-idataset_or_cohort_or_dashboard={database_id}"),
     glue::glue("-ioutput={fs::path_ext_remove(output_path)}")
